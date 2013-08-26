14e7cd0cd8cbc1509bce1b59b38b63eb72ef5d36
Adam Pohorecki <adam.pohorecki@llp.pl>
2013-08-26 15:31:12 +0200

    Add page to dsl methods

--- lib/capybara/angular/dsl.rb
+++ lib/capybara/angular/dsl.rb
@@ -5,16 +5,18 @@ module Capybara
 
       Capybara::Session::DSL_METHODS.each do |method|
         define_method(method) do |*args, &block|
-          wait_until_angular_ready
-          Capybara.current_session.send(method, *args, &block)
+          page.send(method, *args, &block)
         end
+      end
+
+      def page
+        wait_until_angular_ready
+        Capybara.current_session
       end
 
       def wait_until_angular_ready
         Waiter.new(Capybara.current_session).wait_until_ready
       end
-
-      extend DSL
     end
   end
 end
