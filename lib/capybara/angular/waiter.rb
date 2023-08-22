e8422af49fbc5785569a0702b28024049926f6aa
Rubocop Challenger <robot@payrollhero.com>
2023-08-22 23:35:02 +0000

    :police_car: Style/ExpandPathArguments

--- lib/capybara/angular/waiter.rb
+++ lib/capybara/angular/waiter.rb
@@ -1,7 +1,7 @@
 module Capybara
   module Angular
     class Waiter
-      WAITER_JS = IO.read(File.expand_path "../waiter.js", __FILE__)
+      WAITER_JS = IO.read(File.expand_path 'waiter.js', __dir__)
 
       attr_accessor :page
 
